bfd1b2108cec94ec2d79aa09e914d795ada78ca1
Sergey <isa86box@gmail.com>
2021-05-17 15:09:00 +0300

    скрипт для плавной смены экрана "история" на экран "призы"

--- source/js/modules/menu.js
+++ source/js/modules/menu.js
@@ -16,11 +16,35 @@ export default () => {
   }
 
   for (let i = 0; i < menuLinks.length; i++) {
-    menuLinks[i].addEventListener(`click`, function () {
+    menuLinks[i].addEventListener(`click`, function (evt) {
       if (window.innerWidth < 1025) {
         header.classList.remove(`page-header--menu-opened`);
         document.body.classList.remove(`menu-opened`);
       }
+
+      animateScreenChange(evt);
     });
+  }
+};
+
+const animateScreenChange = (evt) => {
+  if (window.location.hash) {
+    const currentHash = window.location.hash;
+
+    if (currentHash === `#story` && evt.target.hash === `#prizes`) {
+      evt.preventDefault();
+
+      const storyScreenElem = document.getElementById(`story`);
+      const storyScreenLink = document.querySelector(`[data-href='story']`);
+
+      storyScreenElem.classList.remove(`active`);
+      storyScreenLink.classList.remove(`active`);
+      storyScreenElem.classList.add(`screen-change`);
+
+      setTimeout(() => {
+        storyScreenElem.classList.remove(`screen-change`);
+        window.location.hash = `#prizes`;
+      }, 650);
+    }
   }
 };
