981ba9f7e82f841302d6a00f95e19cb21738ed11
Sergey <isa86box@gmail.com>
2021-05-30 14:37:45 +0300

    экран "история": небольшой рефакторинг скрипта инициализации слайдера

--- source/js/modules/slider.js
+++ source/js/modules/slider.js
@@ -3,9 +3,15 @@ import Swiper from "swiper";
 export default () => {
   let storySlider;
   let sliderContainer = document.getElementById(`story`);
-  sliderContainer.style.backgroundImage = `url("img/slide1.jpg"), linear-gradient(180deg, rgba(83, 65, 118, 0) 0%, #523E75 16.85%)`;
+
+  const slide1Background = `url("img/slide1.jpg"), linear-gradient(180deg, rgba(83, 65, 118, 0) 0%, #523E75 16.85%)`;
+  const slide2Background = `url("img/slide2.jpg"), linear-gradient(180deg, rgba(45, 54, 179, 0) 0%, #2A34B0 16.85%)`;
+  const slide3Background = `url("img/slide3.jpg"), linear-gradient(180deg, rgba(92, 138, 198, 0) 0%, #5183C4 16.85%)`;
+  const slide4Background = `url("img/slide4.jpg"), linear-gradient(180deg, rgba(45, 39, 63, 0) 0%, #2F2A42 16.85%)`;
 
   const setSlider = function () {
+    setBackgroundImage();
+
     if (((window.innerWidth / window.innerHeight) < 1) || window.innerWidth < 769) {
       storySlider = new Swiper(`.js-slider`, {
         pagination: {
@@ -17,15 +23,7 @@ export default () => {
         },
         on: {
           slideChange: () => {
-            if (storySlider.activeIndex === 0 || storySlider.activeIndex === 1) {
-              sliderContainer.style.backgroundImage = `url("img/slide1.jpg"), linear-gradient(180deg, rgba(83, 65, 118, 0) 0%, #523E75 16.85%)`;
-            } else if (storySlider.activeIndex === 2 || storySlider.activeIndex === 3) {
-              sliderContainer.style.backgroundImage = `url("img/slide2.jpg"), linear-gradient(180deg, rgba(45, 54, 179, 0) 0%, #2A34B0 16.85%)`;
-            } else if (storySlider.activeIndex === 4 || storySlider.activeIndex === 5) {
-              sliderContainer.style.backgroundImage = `url("img/slide3.jpg"), linear-gradient(180deg, rgba(92, 138, 198, 0) 0%, #5183C4 16.85%)`;
-            } else if (storySlider.activeIndex === 6 || storySlider.activeIndex === 7) {
-              sliderContainer.style.backgroundImage = `url("img/slide4.jpg"), linear-gradient(180deg, rgba(45, 39, 63, 0) 0%, #2F2A42 16.85%)`;
-            }
+            setBackgroundImage(storySlider.activeIndex);
           },
           resize: () => {
             storySlider.update();
@@ -51,15 +49,7 @@ export default () => {
         },
         on: {
           slideChange: () => {
-            if (storySlider.activeIndex === 0) {
-              sliderContainer.style.backgroundImage = `url("img/slide1.jpg")`;
-            } else if (storySlider.activeIndex === 2) {
-              sliderContainer.style.backgroundImage = `url("img/slide2.jpg")`;
-            } else if (storySlider.activeIndex === 4) {
-              sliderContainer.style.backgroundImage = `url("img/slide3.jpg")`;
-            } else if (storySlider.activeIndex === 6) {
-              sliderContainer.style.backgroundImage = `url("img/slide4.jpg")`;
-            }
+            setBackgroundImage(storySlider.activeIndex);
           },
           resize: () => {
             storySlider.update();
@@ -68,6 +58,27 @@ export default () => {
         observer: true,
         observeParents: true
       });
+    }
+  };
+
+  const setBackgroundImage = (index = 0) => {
+    switch (index) {
+      case 0:
+      case 1:
+        sliderContainer.style.backgroundImage = slide1Background;
+        break;
+      case 2:
+      case 3:
+        sliderContainer.style.backgroundImage = slide2Background;
+        break;
+      case 4:
+      case 5:
+        sliderContainer.style.backgroundImage = slide3Background;
+        break;
+      case 6:
+      case 7:
+        sliderContainer.style.backgroundImage = slide4Background;
+        break;
     }
   };
 
