306f7b5b35b2b88fef294090de11faa3d7227678
Sergey <isa86box@gmail.com>
2021-08-06 13:25:17 +0300

    снизил частоту смены кадров в анимации количества призов

--- source/js/modules/helper.js
+++ source/js/modules/helper.js
@@ -1,18 +1,28 @@
 export const animateDigits = (elem, initVal, lastVal, duration) => {
+  const fps = 12;
   let startTime = null;
+  let lastTime = null;
+  let fpsInterval = 1000 / fps;
 
   const step = (currentTime) => {
     if (!startTime) {
       startTime = currentTime;
+      lastTime = currentTime;
     }
-    const progress = Math.min((currentTime - startTime) / duration, 1);
-    elem.innerHTML = Math.floor(progress * (lastVal - initVal) + initVal);
 
-    if (progress < 1) {
-      window.requestAnimationFrame(step);
-    } else {
-      window.cancelAnimationFrame(window.requestAnimationFrame(step));
+    let timePassed = currentTime - lastTime;
+
+    if (timePassed > fpsInterval) {
+      const progress = Math.min((currentTime - startTime) / duration, 1);
+      elem.innerHTML = Math.floor(progress * (lastVal - initVal) + initVal);
+
+      lastTime = currentTime;
+
+      if (progress >= 1) {
+        window.cancelAnimationFrame(window.requestAnimationFrame(step));
+      }
     }
+    window.requestAnimationFrame(step);
   };
 
   window.requestAnimationFrame(step);
