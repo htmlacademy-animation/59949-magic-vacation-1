d51495897464f8357a117ec9a683ba5abfa6fcb2
Keks <keks@htmlacademy.ru>
2021-08-07 08:12:54 +0300

    :heavy_check_mark: Сборка #12

--- 12/js/script.js
+++ 12/js/script.js
@@ -10722,20 +10722,30 @@ class Timer {
 __webpack_require__.r(__webpack_exports__);
 /* harmony export (binding) */ __webpack_require__.d(__webpack_exports__, "animateDigits", function() { return animateDigits; });
 const animateDigits = (elem, initVal, lastVal, duration) => {
+  const fps = 12;
   let startTime = null;
+  let lastTime = null;
+  let fpsInterval = 1000 / fps;
 
   const step = (currentTime) => {
     if (!startTime) {
       startTime = currentTime;
+      lastTime = currentTime;
     }
-    const progress = Math.min((currentTime - startTime) / duration, 1);
-    elem.innerHTML = Math.floor(progress * (lastVal - initVal) + initVal);
 
-    if (progress < 1) {
-      window.requestAnimationFrame(step);
-    } else {
-      window.cancelAnimationFrame(window.requestAnimationFrame(step));
+    let timePassed = currentTime - lastTime;
+
+    if (timePassed > fpsInterval) {
+      const progress = Math.min((currentTime - startTime) / duration, 1);
+      elem.innerHTML = Math.floor(progress * (lastVal - initVal) + initVal);
+
+      lastTime = currentTime;
+
+      if (progress >= 1) {
+        window.cancelAnimationFrame(window.requestAnimationFrame(step));
+      }
     }
+    window.requestAnimationFrame(step);
   };
 
   window.requestAnimationFrame(step);
